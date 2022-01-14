b250b14a5f6c103aceb50617d53e9509605be5a3
Krfld <rodrigofldias@gmail.com>
2022-01-14 17:46:32 +0000

    .

--- 05_WorkTerm_TrabalhoFinal/_AMD_FinalProject_B/remove_useless_products.py
+++ 05_WorkTerm_TrabalhoFinal/_AMD_FinalProject_B/remove_useless_products.py
@@ -1,14 +1,11 @@
-from ast import For
+import numpy as np
 import pandas as pd
 
-ignoreProducts = ['home', 'open', '/customer/', 'display.category*homepage']
+ignoreProducts = ['home', 'open', '/customer', 'display.category*homepage']
 
 if __name__ == '__main__':
     df = pd.read_csv('z_dataset_JAN_updated.csv')
-    out = pd.DataFrame(columns=df.columns)
-    for row in df.values:
-        if not str(row[4]) in ignoreProducts:
-            out = out.append([row], ignore_index=True)
-
-    # print(out)
-    out.to_csv('z_dataset_JAN_updated_filtered.csv', index=False)
+    df = df[df['product_gui'] != '']
+    for productToIgnore in ignoreProducts:
+        df = df[df['product_gui'].str.startswith(productToIgnore) == False]
+    df.to_csv('z_dataset_JAN_updated_filtered.csv', index=False)
