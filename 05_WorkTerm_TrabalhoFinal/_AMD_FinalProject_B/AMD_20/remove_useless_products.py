dcfa586ae5a185f9933e69158f8cd24ca312e679
Krfld <rodrigofldias@gmail.com>
2022-01-17 23:22:03 +0000

    .

--- 05_WorkTerm_TrabalhoFinal/_AMD_FinalProject_B/AMD_20/remove_useless_products.py
+++ 05_WorkTerm_TrabalhoFinal/_AMD_FinalProject_B/AMD_20/remove_useless_products.py
@@ -5,8 +5,11 @@ ignoreProducts = ['home', 'open', '/customer', 'display.category*homepage']
 
 # Removes the rows where product_gui starts with any of the ignoreProducts
 if __name__ == '__main__':
-    df = pd.read_csv('z_dataset_JAN_updated.csv')
+    fileName = 'z_dataset_JAN_updated'
+
+    df = pd.read_csv(fileName + '.csv')
     df = df[df['product_gui'] != '']
     for productToIgnore in ignoreProducts:
         df = df[df['product_gui'].str.startswith(productToIgnore) == False]
-    df.to_csv('z_dataset_JAN_updated_filtered.csv', index=False)
+
+    df.to_csv(fileName + '_filtered.csv', index=False)
